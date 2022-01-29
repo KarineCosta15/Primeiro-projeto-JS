0115ef93ede17b8db54e07e5665e2d19c470895f
Karine Costa <karineacosta15@gmail.com>
2022-01-29 18:39:42 -0300

    Add files via upload

---
+++ Arquivos/javascript.js
@@ -0,0 +1,12 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function aumenta() {
+  currentNumber = currentNumber + 1;  
+  currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function diminui() {
+    currentNumber = currentNumber - 1;  
+    currentNumberWrapper.innerHTML = currentNumber;
+  }
